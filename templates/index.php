d3358d855676bd94337b6968a581271343284186
Archey Barrell <archey.barrell@outlook.com>
2024-09-25 23:52:13 +0100

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -4,9 +4,21 @@
 </div>
 </p>
 <p>
-      <ion-icon name="logo-linkedin"></ion-icon> <a href="https://www.linkedin.com/in/archey-barrell/">archey-barrell</a>
+      Hi 👋<br>
+      <br>
+      You've reached the website of Archey Barrell.<br> 
+      <br>
+      I'm currently working as a software engineer at <a class="link" href="https://netcraft.com">Netcraft</a>, a cybercrime disruption company.<br>
+      <br>
+      Outside of work, I am intrested in <a class="link" href="https://archbar.me/photos/">photography</a> and <a class="link" href="https://wwlrc.co.uk">off-road motorsport</a>. I also spend a lot of time volunteering at the Scouts <a class="link" href="https://instagram.com/51stBathScouts/">as a leader</a>, amongst other things.<br>
 </p>
 <p>
-      <ion-icon name="logo-github"></ion-icon> <a href="https://github.com/archey347">archey347</a><br><br>
+      <h3>Socials</h3>
+      <span class="margin: 15px;"><ion-icon name="logo-linkedin"></ion-icon> <a href="https://www.linkedin.com/in/archey-barrell/">archey-barrell</a></span><br>
+      <span class="margin: 15px;"><ion-icon name="logo-github"></ion-icon> <a href="https://github.com/archey347">archey347</a></span><br>
+      <span class="margin: 15px;"><ion-icon name="logo-instagram"></ion-icon> <a href="https://instagram.com/archeybarnel">archeybarnel</a></span><br>
+</p>
+<p>
+      <h3>Code Reviews</h3>
       If I have reviewed one of your pull requests, I would recommend taking a read of my <u><a href="/code_review.php">code review guide</a></u>.
 </p>
