f258ddd5488a5f3227280a43542d022039e56f61
Archey Barrell <archey.barrell@outlook.com>
2024-11-10 00:52:46 +0000

    Update contact.php

--- templates/contact.php
+++ templates/contact.php
@@ -19,6 +19,6 @@ My GPG key is available <a target="_blank" class="link" href="https://archbar.me
 
 <p>
 
-<code>3079 5E64 9CE8 0861 560A 2A93 7B82 434F 2973 8AB5</code>
+<code style="padding: 10px;">3079 5E64 9CE8 0861 560A 2A93 7B82 434F 2973 8AB5</code>
 
 </p>
